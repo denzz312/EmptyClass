62154c2b33325a60946f412ca298f9e85a4fd4be
Brandon Ma <ma.brandonjh@gmail.com>
2021-12-01 15:15:12 -0500

    Auto stash before merge of "master" and "origin/master"

--- src/App.js
+++ src/App.js
@@ -2,28 +2,26 @@ import {render} from '@testing-library/react';
 import logo from '../src/logo192.png';
 import './App.css';
 import React from "react";
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Header from './Components/Header';
 import BodyComponent from './Components/BodyComponent';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
+import SearchResults from './Components/SearchResults';
 import JoinOthers from "./Components/JoinOthers";
 
 class App extends React.Component {
     render() {
 
-        return (
-            <div className="App">
-                <Header/>
-                <body className="App-body">
-
-                {/*<Home/>*/}
-                <JoinOthers />
-                </body>
-                <Footer/>
-            </div>
-        );
-    }
+    return (
+      <div className="App">
+        <Header/>
+        <body className="App-body">
+        <SearchResults/>
+        </body>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/SearchResults.js
+++ src/Components/SearchResults.js
@@ -1,17 +1,54 @@
 import React from "react";
 
-class SearchResults extends React.Component{
- 
-    render(){
-    
-    return(
-        <div>
-            css for any component goes into 'App.css'
-            This is where you put the html/forms
-        </div>
-        
-    );
-    
+class SearchResults extends React.Component {
+
+    handleJoin() {
+        console.log('clicked');
+    }
+
+    render() {
+
+        return (
+            <div>
+                <div>
+                    Available Spaces
+                    <input
+                        type="text"
+                        id="header-search"
+                        placeholder="Search"
+                        name="s"
+                    />
+                </div>
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Room #</th>
+                                <th>Building Details</th>
+                                <th>Selection</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>2.125</th>
+                                <th>JMSB Building</th>
+                                <th>
+                                    <button>Select Room</button>
+                                </th>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div>
+                    <button className="ReturnButton"
+                        onClick={this.handleJoin}>
+                        Return
+                    </button>
+                </div>
+            </div>
+
+        );
+
     }
 }
 
