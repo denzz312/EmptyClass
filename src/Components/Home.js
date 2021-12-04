0fb245af2031c461a49833f6a712c390dc965478
Mateo <mateo@aheeva.com>
2021-12-04 14:09:53 -0500

    getting data

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,128 @@
 import React from "react";
 import { FaUsers, FaSearch } from "react-icons/fa";
-import JoinOthers from "./JoinOthers";
+import Data from "./compieddata.json";
 
 class Home extends React.Component{
 
+  constructor(props) {
+    super(props);
+    this.state = {
+        day: "monday",
+        startTime: "07:00",
+        endTime: "08:00",
+        searchData: [],
+    };
+    
+    this.handleDay = this.handleDay.bind(this);
+    this.handleStart = this.handleStart.bind(this);
+    this.handleEnd = this.handleEnd.bind(this);
+    this.handleJoin = this.handleJoin.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+}
  
-    handleJoin(){
-      console.log('clicked');
+
+    handleDay = (event) =>{
+      this.setState({day: event});
+    }
+
+    handleStart = (event) =>{
+      this.setState({startTime: event.target.value});
+    }
+    
+    handleEnd = (event) =>{
+      this.setState({endTime: event.target.value});
+    }
+    
+    handleSearch = (e) =>{
+      e.preventDefault();
+      //checks that we get data from inputs
+      //console.log(this.state);
+      
+      {Data.map(post => {
+        //how I will parse dates
+       //const test = post.H["920"]["monday"][0];
+       //let start = test[0];
+       //start = start.replace('.00','');
+       //start = start.replace('.','');
+       //if(start.charAt(0) == '0'){
+       //  console.log(start.charAt(0));
+       //}
+
+       //let end = test[1];
+       //end = end.replace('.00','');
+       //console.log(end.replace('.',''));
+
+        let classList = post.H;
+        let searchList = [];
+        
+        this.state.startTime = this.state.startTime.replace(":",'');
+        let sTime = this.state.startTime;
+        if(sTime.charAt(0) == '0'){
+           sTime = sTime.slice(1);
+        }
+        //console.log(sTime);
+        
+        this.state.endTime = this.state.endTime.replace(":",'');
+        let eTime = this.state.endTime;
+        if(eTime.charAt(0) == '0'){
+          eTime = eTime.slice(1);
+        }
+        //console.log(eTime);
+
+        //this should be the loop to finish this
+        for(var i = 0; i < 1271; i++){
+          if(classList[i] != null){
+            console.log(i);
+           for(var j = 0; j < 10; j++){
+            //this should allow only days that have free time be be added into list
+            if((classList[i][this.state.day][j] != null) ||
+            (classList[i][this.state.day][j] != undefined)){
+              
+              //adjust value of schedule start to be calculated
+              let scStart = classList[i][this.state.day][j][0].replace(".00","");
+              scStart = scStart.replace(".","");
+              if(scStart.charAt(0) == '0'){
+                scStart = scStart.slice(1);
+              }
+
+              //adjust value of schedule end to be calculated
+              let scEnd = classList[i][this.state.day][j][1].replace(".00","");
+              scEnd = scEnd.replace(".","");
+              if(scEnd.charAt(0) == '0'){
+                scEnd = scEnd.slice(1);
+              }
+
+              //add room number to list to pass to SearchResults
+              if(parseInt(sTime) >= parseInt(scStart) &&
+                 parseInt(eTime) <= parseInt(scEnd)){
+                   console.log("works")
+                 console.log(scStart);
+                 console.log(scEnd);
+                 searchList.push(i);
+                 console.log(searchList);
+                }
+            }
+           }
+          }
+        }
+
+       return(
+        <>
+{/*         
+       <h3>Start</h3><h2>{start}</h2>
+       <h3>End</h3><h2>{end}</h2>
+       <h2>{array}</h2> */}
+
+        </>
+     )}
+     )
+    }
+
+    }
+
+    handleJoin = (e) =>{
+      e.preventDefault();
+      console.log(this.state);
     }
 
     render(){
@@ -19,21 +135,21 @@ class Home extends React.Component{
                 <div>
                   <label for="ev">EV Building</label>
                   <input type="checkbox"
-                         name="favorite_pet"
-                         value="ev"/>
+                         name="ev"
+                         disabled/>
                 </div>
                 <div>
                   <label for="jmsb">JMSB Building</label>
                   <input type="checkbox"
-                         name="favorite_pet"
-                         value="jsmb"/>
+                         name="jmsb"
+                         disabled/>
                 </div>
 
                 <div>
                   <label for="hall">Hall Building</label>
                   <input type="checkbox"
-                         name="favorite_pet"
-                         value="hall"/>
+                         name="hall"
+                         checked/>
                 </div>
               </div>
               <br/>
@@ -43,10 +159,10 @@ class Home extends React.Component{
                       <label for="weekday">Choose Day</label>
                     </div>
                     <div>
-                      <select id="weekday" name="weekday">
-                        <option value="monday">monday</option>
+                      <select id="weekday" name="weekday" onSelect={this.handleDay}>
+                        <option value="monday" selected>monday</option>
                         <option value="tuesday">tuesday</option>
-                        <option value="wednesday">wednesday</option>
+                        <option value="wednesday" >wednesday</option>
                         <option value="thursday">thursday</option>
                         <option value="friday">friday</option>
                       </select>
@@ -63,9 +179,10 @@ class Home extends React.Component{
                     <input
                       className="inputStyle"
                       type="text"
-                      placeholder="HH:MM"
+                      placeholder="07:15"
                       name="Start Time"
-                      onChange={this.handleChange}
+                      value={this.state.startTime}
+                      onChange={this.handleStart}
                     />
                     </div>
                 </div>
@@ -80,9 +197,10 @@ class Home extends React.Component{
                     <input
                       className="inputStyle"
                       type="text"
-                      placeholder="HH:MM"
+                      placeholder="08:30"
                       name="End Time"
-                      onChange={this.handleChange}
+                      value={this.state.endTime}
+                      onChange={this.handleEnd}
                     />
                     </div>
                 </div>
@@ -91,7 +209,8 @@ class Home extends React.Component{
               <div>
                 <button
                   className="buttons"
-                  onClick={this.handleJoin}>
+                  onClick={this.handleSearch}
+                  >
                   Search
                   &nbsp;
                   <FaSearch/>
@@ -106,13 +225,43 @@ class Home extends React.Component{
           <div>
             <button
               className="buttons"
-              onClick={this.handleJoin}>
+              onClick={this.handleSearch}
+              >
               Join Others
               &nbsp;
               <FaUsers/>
             </button>
           </div>
-        </div>
+          {/* <div>
+            {Data.map(post => {
+
+                const test = post.H["920"]["monday"][0];
+                // test = purpose.replace(/./g,"");
+                let start = test[0].split(".");
+                let end = test[1].split(".");
+                let app = post;
+                let apple = console.log(app);
+                let newData = []
+
+                Object.keys(app).forEach(function(key){
+                  newData.push(app[key]);
+                });
+
+                let array = console.log(newData)
+
+               return(
+                <>
+                
+               <h3>Start</h3><h2>{start}</h2>
+               <h3>End</h3><h2>{end}</h2>
+               <h2>{array}</h2>
+
+                </>
+             )}
+             )
+            }
+          </div> */}
+          </div>
     );
     
     }
